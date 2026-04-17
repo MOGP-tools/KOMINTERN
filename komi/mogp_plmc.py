2141c1147b1552a1161ee8ce0f6fa3e73d482db5
OLIVIER TRUFFINET <olivier.truffinet@edf.fr>
2026-04-17 18:21:23 +0200

    Suppression de .div_ problématiques

--- komi/mogp_plmc.py
+++ komi/mogp_plmc.py
@@ -640,7 +640,7 @@ class ProjectedLMCmll(gp.mlls.ExactMarginalLogLikelihood):
         # Get the log prob of the marginal distribution of latent processes
         latent_output = self.likelihood(latent_function_dist, *params) # shape (n_batch x) n_latents x n_points
         latent_res = latent_output.log_prob(proj_target)
-        latent_res = self._add_other_terms(latent_res, params).sum().div_(num_data)  # Scale by the amount of data we have
+        latent_res = self._add_other_terms(latent_res, params).sum()/ num_data  # Scale by the amount of data we have
 
         # compute the part of likelihood lost by projection
         p, q = self.model.n_tasks, self.model.n_latents
@@ -652,7 +652,7 @@ class ProjectedLMCmll(gp.mlls.ExactMarginalLogLikelihood):
                 log_B_tilde = self.model.log_B_tilde
                 B_tilde_inv_val = torch.exp(- log_B_tilde[..., 0])
                 log_B_tilde_root_diag = log_B_tilde / 2
-                self.proj_term_list[1] = ( B_tilde_inv_val * (self.model.Y_squared_norm - torch.linalg.matrix_norm(target @ Q)) ).sum().div_(num_data)
+                self.proj_term_list[1] = ( B_tilde_inv_val * (self.model.Y_squared_norm - torch.linalg.matrix_norm(target @ Q)) ).sum()/ num_data
                 # the parenthesis is the squared norm of the projection of target onto the space orthogonal to span(Q)
             else:
                 self.proj_term_list[1] = 0.
@@ -665,7 +665,7 @@ class ProjectedLMCmll(gp.mlls.ExactMarginalLogLikelihood):
                 B_tilde_inv_root_diag = self.model.B_tilde_inv_chol[..., range(p-q), range(p-q)]
                 log_B_tilde_root_diag = -torch.log(B_tilde_inv_root_diag)
                 rot_proj_scaled_target = target @ Q_orth @ self.model.B_tilde_inv_chol
-            self.proj_term_list[1] = torch.linalg.matrix_norm(rot_proj_scaled_target).div_(num_data)
+            self.proj_term_list[1] = torch.linalg.matrix_norm(rot_proj_scaled_target)/ num_data
 
         # All terms are implicitly or explicitly divided by the number of datapoints
         self.proj_term_list[0] = 2 * torch.sum(log_B_tilde_root_diag) # factor 2 because of the use of a root
